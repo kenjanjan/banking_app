8c6b423ee34e1760cd38d513a78ac039f575ef30
kenjanjan <kjliloc@hcdc.edu.ph>
2024-05-04 19:17:23 +0800

    #basic sidebar navigation with responsive design

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import SideBar from "@/components/SideBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Kent', lastName: 'Liloc'}
   return (
-   <main>
-     SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+     <SideBar user={loggedIn}/>
      {children}
    </main>
   );
